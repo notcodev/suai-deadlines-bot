89f326fbb5c36bb239c430656c6853bb0f8a8cc9
Erik <slmv_e@icloud.com>
2024-10-01 07:43:31 +0300

    implemented base handlers

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import { Composer, session, Telegraf } from "telegraf";
+import * as suai from "./suai/index.js";
+import { stage } from "./scenes/index.js";
+import { connectToDatabase, db } from "../drizzle/connection.js";
+import { credentialsAddedOnlyMiddleware } from "./middlewares/credentials-added-only.js";
+import { mainMenuKeyboard } from "./keyboards/main-menu.js";
+import { greetingMessage } from "./messages/greeting.js";
+import { credentials, subscriptions } from "../drizzle/schema.js";
+import { eq } from "drizzle-orm";
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const composer = new Composer();
+
+composer.start(async (ctx) => {
+  await ctx.replyWithMarkdownV2(greetingMessage, {
+    reply_markup: {
+      inline_keyboard: mainMenuKeyboard,
+    },
+  });
+});
+
+composer.action("main_menu", async (ctx) => {
+  await ctx.editMessageText(greetingMessage, {
+    reply_markup: {
+      inline_keyboard: mainMenuKeyboard,
+    },
+    parse_mode: "MarkdownV2",
+  });
+});
+
+composer.action("add_credentials", (ctx) => {
+  return ctx.scene.enter("ADD_CREDENTIALS_SCENE");
+});
+
+composer.use(credentialsAddedOnlyMiddleware);
+composer.action("chats", async (ctx) => {
+  const result = await db
+    .select({
+      id: subscriptions.id,
+      displayName: subscriptions.displayName,
+      chatId: subscriptions.chatId,
+    })
+    .from(subscriptions)
+    .innerJoin(credentials, eq(credentials.id, subscriptions.credentialsId))
+    .where(eq(credentials.userId, ctx.from.id));
+
+  await ctx.editMessageText(
+    `⬇️ *Выберите один из имеющихся чатом или добавьте новый*`,
+    {
+      reply_markup: {
+        inline_keyboard: result
+          .map((entry) => [
+            {
+              text: `${entry.displayName} – ${entry.chatId} ${ctx.chat.id === entry.chatId ? "(Этот чат)" : ""}`,
+              callback_data: `open_chat:${entry.id}`,
+            },
+          ])
+          .concat([[{ text: "Добавить чат", callback_data: "add_chat" }]]),
+      },
+      parse_mode: "MarkdownV2",
+    },
+  );
+});
+
+bot.use(session());
+bot.use(stage.middleware());
+bot.use(Composer.privateChat(composer));
+
+await connectToDatabase();
+bot.launch(() => console.log("Bot successfully started"));
