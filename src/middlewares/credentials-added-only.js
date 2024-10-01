fc48f38f8335765edfd79a7281e27ba92fa63134
Erik <slmv_e@icloud.com>
2024-10-01 07:41:28 +0300

    implemented credentials added only middleware

---
+++ src/middlewares/credentials-added-only.js
@@ -0,0 +1,34 @@
+import { eq } from "drizzle-orm";
+import { db } from "../../drizzle/connection.js";
+import { credentials } from "../../drizzle/schema.js";
+
+export async function credentialsAddedOnlyMiddleware(ctx, next) {
+  const data = await db
+    .select()
+    .from(credentials)
+    .where(eq(credentials.userId, ctx.from.id));
+
+  if (!data[0]) {
+    await ctx.editMessageText(
+      `😢 *Отсутствуют данные для входа*
+
+Для того, чтобы продолжить необходимо добавить данные для входа`,
+      {
+        parse_mode: "MarkdownV2",
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "Добавить данные для входа",
+                callback_data: "add_credentials",
+              },
+            ],
+          ],
+        },
+      },
+    );
+    return;
+  }
+
+  next();
+}
