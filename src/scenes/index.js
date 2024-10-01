41fb429d3a9d676d8a0196718d49e2d0796ab18f
Erik <slmv_e@icloud.com>
2024-10-01 07:45:22 +0300

    changed text and added keyboard for cancel action

--- src/scenes/index.js
+++ src/scenes/index.js
@@ -1,8 +1,16 @@
 import { Scenes } from "telegraf";
 import { addCredentialsScene } from "./add-credentials.js";
+import { greetingMessage } from "../messages/greeting.js";
+import { mainMenuKeyboard } from "../keyboards/main-menu.js";
 
 export const stage = new Scenes.Stage([addCredentialsScene]);
 
 stage.action("cancel", (ctx) =>
-  Promise.all([ctx.scene.leave(), ctx.editMessageText("Отменено")]),
+  Promise.all([
+    ctx.scene.leave(),
+    ctx.editMessageText(greetingMessage, {
+      reply_markup: { inline_keyboard: mainMenuKeyboard },
+      parse_mode: "MarkdownV2",
+    }),
+  ]),
 );
